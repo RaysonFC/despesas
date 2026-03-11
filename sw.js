d50bba9352a921647cefe461ebef754513fff40e
RAYSON FERREIRA CRUZ <167266574+RaysonFC@users.noreply.github.com>
2026-03-11 14:51:19 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+// Desinstala todos os caches antigos
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
